06e35c6a1a8db7ff53dc6468c9c366d7a75195e7
glennm <glenn@arlivesystems.com>
2021-08-03 16:31:30 +0100

    Revert "initial commit"
    This reverts commit b281d97eceed15f3f59a9927e367982c9f48a91d.

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,15 @@
 <template>
-  <h1>Reaction Timer</h1>
-  <button @click="start" :disabled="isPlaying">Start Game</button>
-  <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
-  <Results v-if="showResults" :score="score"/>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import Block from "@/components/Block";
-import Results from "@/components/Results";
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {Results, Block},
-  data() {
-    return {
-      isPlaying: false,
-      delay: null,
-      score: null,
-      showResults: false
-    }
-  },
-  methods: {
-    start() {
-      this.delay = 2000 + Math.random() * 5000
-      this.isPlaying = true;
-      this.showResults = false;
-    },
-    endGame(reactionTime) {
-      this.score = reactionTime;
-      this.isPlaying = false;
-      this.showResults = true;
-    }
+  components: {
+    HelloWorld
   }
 }
 </script>
@@ -41,24 +20,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #444;
+  color: #2c3e50;
   margin-top: 60px;
-}
-
-button {
-  background: #0faf87;
-  color: white;
-  border: none;
-  padding: 8px 16px;
-  border-radius: 4px;
-  font-size: 16px;
-  letter-spacing: 1px;
-  cursor: pointer;
-  margin: 10px;
-}
-
-button[disabled] {
-  opacity: 0.2;
-  cursor: not-allowed;
 }
 </style>
